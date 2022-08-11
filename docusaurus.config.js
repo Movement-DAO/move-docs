a9f22e6399f0a31d855079c9c3f9949e243d0879
filipv <filipeth@protonmail.com>
2022-08-11 02:34:31 -0400

    fix build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Move Docs',
   tagline: 'Run your Movement.',
-  url: 'docs.move.xyz',
+  url: 'https://docs.move.xyz',
   baseUrl: '/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
